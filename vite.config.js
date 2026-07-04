c9752cb80256f120ab8ee3e8cd2c77bbf43d5c59
kuuhaku3614 <113129716+kuuhaku3614@users.noreply.github.com>
2026-07-04 18:10:11 +0800

    update 1.1

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const isUserPage = repositoryName?.endsWith('.github.io');
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.GITHUB_REPOSITORY
-    ? isUserPage
-      ? '/'
-      : `/${repositoryName}/`
-    : '/',
+  base: '/Digital-portfolio/',
 });
